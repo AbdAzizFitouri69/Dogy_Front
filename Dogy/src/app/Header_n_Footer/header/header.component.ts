25b2ddf72e044d6bea5e4a8849fa8da310c57adb
AbdAzizFitouri69 <mohamedabdaziz.fitouri@esprit.tn>
2021-12-19 20:27:02 +0100

    Slider

--- Dogy/src/app/Header_n_Footer/header/header.component.ts
+++ Dogy/src/app/Header_n_Footer/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog : MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  openDialog() {
+    this.dialog.open(LoginDialog);
+  }
+
 }
+
+@Component({
+  selector: 'dialog-elements-example-dialog',
+  templateUrl: 'login.dialog.html',
+})
+export class LoginDialog {
+
+
+
+}
+
--- Dogy/src/app/app.module.ts
+++ Dogy/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OwlModule } from 'ngx-owl-carousel'; 
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header_n_Footer/header/header.component';
 import { FooterComponent } from './Header_n_Footer/footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
